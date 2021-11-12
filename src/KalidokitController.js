7ee2c9a19ad05582851c9f9c1ae38b7cb79c0218
zk-phi <zk-phi@users.noreply.github.com>
2021-11-12 18:44:35 +0900

    Support facemesh

--- src/KalidokitController.js
+++ src/KalidokitController.js
@@ -1,6 +1,7 @@
 import { Quaternion, Euler, Vector3 } from "three";
 import * as Kalidokit from "kalidokit";
 import { Holistic } from "@mediapipe/holistic";
+import { FaceMesh } from "@mediapipe/face_mesh";
 import { Camera } from "@mediapipe/camera_utils";
 import { VRMSchema } from "@pixiv/three-vrm";
 
@@ -10,18 +11,28 @@ const clamp = Kalidokit.Utils.clamp;
 export default class KalidokitController {
   constructor (vrm) {
     this.vrm = vrm;
-    const holistic = new Holistic({ locateFile: file => `/holistic/${file}` });
-    holistic.setOptions({
+    // const detector = new FaceMesh({ locateFile: file => `/face_mesh/${file}` });
+    // detector.setOptions({
+    //   maxNumFaces: 1,
+    //   minDetectionConfidence: 0.7,
+    //   minTrackingConfidence: 0.7,
+    //   refineFaceLandmarks: true,
+    // });
+    // detector.onResults(result => (
+    //   this.updateState(result.multiFaceLandmarks && result.multiFaceLandmarks[0]
+    // )));
+    const detector = new Holistic({ locateFile: file => `/holistic/${file}` });
+    detector.setOptions({
       modelComplexity: 1,
       smoothLandmarks: true,
       minDetectionConfidence: 0.7,
       minTrackingConfidence: 0.7,
       refineFaceLandmarks: true,
     });
-    holistic.onResults(result => this.updateState(result));
+    detector.onResults(result => this.updateState(result.faceLandmarks));
     this.video = document.createElement("video");
     this.camera = new Camera(this.video, {
-      onFrame: async () => await holistic.send({ image: this.video }),
+      onFrame: async () => await detector.send({ image: this.video }),
       width: 320,
       height: 240,
     });
@@ -41,12 +52,9 @@ export default class KalidokitController {
     this.vrm.blendShapeProxy.setValue(VRMSchema.BlendShapePresetName[name], lerpValue);
   }
 
-  updateState (result) {
-    const landmarks = result.faceLandmarks || (
-      result.multiFaceLandmarks && result.multiFaceLandmarks[0]
-    );
-    if (landmarks) {
-      const face = Kalidokit.Face.solve(landmarks, {
+  updateState (faceLandmarks) {
+    if (faceLandmarks) {
+      const face = Kalidokit.Face.solve(faceLandmarks, {
         runtime: "mediapipe",
         video: this.video,
       });
